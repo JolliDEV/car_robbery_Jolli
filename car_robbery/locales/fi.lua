5cc49dcf08eb81ce4cc034c0ed0ebeb3d71f1828
JolliDEV <151402838+JolliDEV@users.noreply.github.com>
2023-11-19 00:53:04 +0200

    car_robbery_Jolli
    Car robbery script
    Fivem
    ESX

---
+++ car_robbery/locales/fi.lua
@@ -0,0 +1,13 @@
+Locales['fi'] = {
+  ['vehicle_robbery'] = 'Tuontiauto!',
+  ['mission_failed'] = 'Ryöstö epäonnistui',
+  ['get_back_car_10s'] = 'Sinulla on 10-sekunntia aikaa palata autolle.',
+  ['get_back_car_1m'] = 'Sinulla on 1-minuutti aika palata autolle',
+  ['car_provided_rule'] = 'Sinun täytyy käyttää ryöstämääsi autoa.',
+  ['already_robbery'] = 'Tuontiauto on jo käynnissä!',
+  ['not_enough_cops'] = 'Ei tarpeeksi poliiseja paikalla!',
+  ['car_stealing_in_progress'] = 'Tuonti auto käynnissä. Signaali on laitettu päälle',
+  ['steal_a_car'] = 'press ~INPUT_CONTEXT~ Ryöstääksesi auto',
+  ['drop_car_off'] = 'press ~INPUT_CONTEXT~ Jättääksesi auto',
+  ['cooldown'] = 'Tuontiauto oli juuri ryöstetty, joku kerkesi rötöstellä ennen sinua.'
+}
